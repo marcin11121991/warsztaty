38bfeb98f3ddc469cd846ee76e0bdd78a0ed6dc1
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-11 15:00:22 +0100

    pierwszy warsztat z js

---
+++ js/warsztat1/js/index.js
@@ -0,0 +1,10 @@
+var liczba;
+
+function iloczynpar(parametr1, parametr2, parametr3){
+    var wynik = parametr1 * parametr2 * parametr3;
+    return (wynik);
+}
+
+var liczba = iloczynpar(5, 5, 5);
+console.log(liczba);
+
