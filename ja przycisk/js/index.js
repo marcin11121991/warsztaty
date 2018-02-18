8c1812d6be52a90f86387a2d828b511134d12add
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-18 10:50:00 +0100

    poczatek walidacji formularza

--- ja przycisk/js/index.js
+++ ja przycisk/js/index.js
@@ -1,16 +1,22 @@
 "use strict";
 
-
-function ustawTlo(){
-    var allParagraphs = document.getElementsByTagName("p");
-    allParagraphs[0].style.backgroundColor="yellow";
-    allParagraphs[1].style.backgroundColor="red";
-}
+//
+//function ustawTlo() {
+//    var allParagraphs = document.getElementsByTagName("p");
+//    allParagraphs[0].style.backgroundColor = "yellow";
+//    allParagraphs[1].style.backgroundColor = "red";
+//}
 
 
 function pobierzImie(event) {
-    event.preventDefault();
+  event.preventDefault();
+    //   console.log(form);
     var form = document.getElementById("formularz");
-    console.log(form);
-    
+    for (var i = 0; i < form.lenght; i++) {
+      //  console.log(form.elements[i].value);
+        if (form.elements[i].value != 'Submit') {
+            console.log(form.elements[i].value);
+        }
+    }
+
 }
